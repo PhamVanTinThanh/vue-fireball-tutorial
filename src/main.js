b8acbda69d6790331bd5ef869b72cc4968838ecf
Pham Van Tin Thanh <phamvantinthanh0102@gmail.com>
2019-04-01 15:44:38 +0700

    put firebase configuration in main.js

--- src/main.js
+++ src/main.js
@@ -8,3 +8,18 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+<script src="https://www.gstatic.com/firebasejs/5.9.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC5LA0gEOp_qBKPolM9Rodh-yikaCRxVyc",
+    authDomain: "vue-firebase-tutorial-2c533.firebaseapp.com",
+    databaseURL: "https://vue-firebase-tutorial-2c533.firebaseio.com",
+    projectId: "vue-firebase-tutorial-2c533",
+    storageBucket: "vue-firebase-tutorial-2c533.appspot.com",
+    messagingSenderId: "688940576480"
+  };
+  firebase.initializeApp(config);
+</script>
