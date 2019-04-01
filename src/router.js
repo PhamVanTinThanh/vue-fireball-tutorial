77bb8a65a8458683b2e083c3e1bf74d0f77af4a3
Pham Van Tin Thanh <phamvantinthanh0102@gmail.com>
2019-04-01 17:13:24 +0700

    require authentication feature

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import SignUp from './views/SignUp.vue'
-Vue.use(Router)
 
-export default new Router({
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import SignUp from '@/views/SignUp'
+
+Vue.use(Router);
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -18,14 +21,6 @@ export default new Router({
       redirect: '/login'
     },
     {
-      path: '/home',
-      name: 'home',
-      component: Home,
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
       path: '/login',
       name: 'Login',
       component: Login
@@ -34,6 +29,25 @@ export default new Router({
       path: '/sign-up',
       name: 'SignUp',
       component: SignUp
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  
+  if(requiresAuth && !currentUser) next('login');
+  else if(!requiresAuth && currentUser) next('home');
+  else next();
+});
+
+export default router;
