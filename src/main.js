b7373c2d261147712708e0851da3b618b55af6de
Pham Van Tin Thanh <phamvantinthanh0102@gmail.com>
2019-04-01 15:51:36 +0700

    import firebase to main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
@@ -7,19 +8,17 @@ Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
 
 
 <script src="https://www.gstatic.com/firebasejs/5.9.2/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC5LA0gEOp_qBKPolM9Rodh-yikaCRxVyc",
-    authDomain: "vue-firebase-tutorial-2c533.firebaseapp.com",
-    databaseURL: "https://vue-firebase-tutorial-2c533.firebaseio.com",
-    projectId: "vue-firebase-tutorial-2c533",
-    storageBucket: "vue-firebase-tutorial-2c533.appspot.com",
-    messagingSenderId: "688940576480"
-  };
-  firebase.initializeApp(config);
-</script>
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC5LA0gEOp_qBKPolM9Rodh-yikaCRxVyc",
+  authDomain: "vue-firebase-tutorial-2c533.firebaseapp.com",
+  databaseURL: "https://vue-firebase-tutorial-2c533.firebaseio.com",
+  projectId: "vue-firebase-tutorial-2c533",
+  storageBucket: "vue-firebase-tutorial-2c533.appspot.com",
+  messagingSenderId: "688940576480"
+};
+firebase.initializeApp(config);
