cee853da70d04c2c434b58b0985d698b13a185ea
Pham Van Tin Thanh <phamvantinthanh0102@gmail.com>
2019-04-01 15:59:54 +0700

    registering functions

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="sign-up">
         <p>Let's create a new account !</p>
-        <input type="text" placeholder="Email"><br>
-        <input type="password" placeholder="Password"><br>
-        <button>Sign Up</button>
+        <input type="text" v-model="email" placeholder="Email"><br>
+        <input type="password" v-model="password" placeholder="Password"><br>
+        <button @click="signUp">Sign Up</button>
         <span>or go back to <router-link to="/sign-up">login.</router-link></span>
     </div>
 </template>
@@ -12,9 +12,23 @@
     export default {
         name: 'signUp',
         data() {
-            return {};
+            return {
+                email: '',
+                password: ''
+            }
         },
-        methods: {}
+        methods: {
+            signUp: function() {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+                    function(user) {
+                        allert('Your account has been created !')
+                    },
+                    function(err) {
+                        alert('Oops. ' + err.message)
+                    }
+                )
+            }
+        }
     }
 </script>
 
